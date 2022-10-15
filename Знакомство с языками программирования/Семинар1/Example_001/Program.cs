dd8d210b011399faf15ebb82eac22c18d52feecc
Дмитрий <dkovaloff@gmail.com>
2022-10-15 19:35:10 +0500

    initial commit

---
+++ Знакомство с языками программирования/Семинар1/Example_001/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Программа вычисления квадрата числа");
+Console.WriteLine("Введите число");
+//1. Ввод числа
+int input = int.Parse(Console.ReadLine()!);
+
+//2. Вычисление квадрата числа
+int result = input * input;
+//3. Вывод результата на консоль
+Console.WriteLine("Квадрат числа: " + result);
